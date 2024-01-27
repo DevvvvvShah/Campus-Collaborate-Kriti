7584a063883c239986a6f03b166aa1a41f9e654d
hikey-dj <hayagrivan.dj@gmail.com>
2024-01-27 12:57:49 +0530

    Sync

---
+++ server/controllers/getSecondaryProfile.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { authenticateUser, verifyToken } = require('../middlewares/verifyToken');
+const router = express.Router();
+
+const getUserProfile = async (req,res) => {
+
+}
