92d7b5e670ab974327e65ed85e0fe3b5ccbc36f5
Dishant Kumar <katariyadishant418@gmail.com>
2024-01-29 00:18:52 +0530

    added project routes

---
+++ server/routes/projectRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { verifyToken, authenticateUser } = require('../middlewares/verifyToken');
+const router = express.Router();
+const { newProject, getAllProjects, getProject, deleteProject, likeProject, dislikeProject, getMyProjects, addComment,getMyConnectionProjects } = require('../controllers/projectController');
+
+
+router.post('/', verifyToken, authenticateUser, newProject);
+router.get('/', getAllProjects);
+router.get('/my', verifyToken, authenticateUser, getMyProjects);
+router.get('/myconnectionprojects', verifyToken, authenticateUser, getMyConnectionProjects);
+router.get('/:projectId', getProject);
+router.delete('/:projectId', verifyToken, authenticateUser, deleteProject); // authorization done in controllers
+router.put('/likes', verifyToken, authenticateUser, likeProject);    
+router.put('/dislikes', verifyToken, authenticateUser, dislikeProject);
+router.post('/comment', verifyToken, authenticateUser, addComment);
+
+module.exports = router;
