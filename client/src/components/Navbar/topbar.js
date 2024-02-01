58699984ed576bc7bd8c80ef04999721b26d88ad
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2024-02-02 04:06:16 +0530

    changes--

--- client/src/components/Navbar/topbar.js
+++ client/src/components/Navbar/topbar.js
@@ -1,62 +1,53 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const Topbar = () => {
-=======
-import { prodDependencies } from 'mathjs';
-import React from 'react';
+import { prodDependencies } from "mathjs";
+import React from "react";
 
 const Topbar = (props) => {
->>>>>>> e6a212492ce49a40957f9a66f30d71178442f5c0
-    const handleSearch = () => {
-        if(window.innerWidth < 768){
-            const search = document.querySelector('input');
-            search.classList.toggle('hidden');
-            const topic = document.querySelector('.topic');
-            const profile = document.querySelector('.profile');
-            const searchIcon = document.querySelector('.icon');
-            if(search.classList.contains('hidden')){
-                topic.classList.remove('hidden');
-                profile.classList.remove('hidden');
-                searchIcon.classList.remove('absolute');
-                search.classList.remove('w-[90vw]');
-            }
-            else{
-                topic.classList.add('hidden');
-                profile.classList.add('hidden');
-                searchIcon.classList.add('absolute');
-                search.classList.add('w-[90vw]');
-            }
-        }
+  const handleSearch = () => {
+    if (window.innerWidth < 768) {
+      const search = document.querySelector("input");
+      search.classList.toggle("hidden");
+      const topic = document.querySelector(".topic");
+      const profile = document.querySelector(".profile");
+      const searchIcon = document.querySelector(".icon");
+      if (search.classList.contains("hidden")) {
+        topic.classList.remove("hidden");
+        profile.classList.remove("hidden");
+        searchIcon.classList.remove("absolute");
+        search.classList.remove("w-[90vw]");
+      } else {
+        topic.classList.add("hidden");
+        profile.classList.add("hidden");
+        searchIcon.classList.add("absolute");
+        search.classList.add("w-[90vw]");
+      }
     }
+  };
 
-    return (
-        <div className="w-full bg-white drop-shadow-md">
-            <div className='md:ml-[25vw] ml-[5vw] flex items-center py-[1.5vh] align-center justify-between'>
-                <div className='topic md:text-xl md:text-lg h-fit pl-[2vw]'>
-<<<<<<< HEAD
-                    Discussion Forum
-=======
-                    {props.title}
->>>>>>> e6a212492ce49a40957f9a66f30d71178442f5c0
-                </div>
-                <div className='flex items-center'>
-                    <div className='relative min-w-[2rem] md:max-w-[20rem]'>
-                        <img src="images/search.svg" alt="Description" 
-                        className="icon md:absolute left-3 top-2 object-cover object-center w-[1rem] h-[1.05rem]" 
-                        onClick={handleSearch}/>
-                        <input
-                            type="text"
-                            className='bg-[#EEE] rounded-full md:block hidden px-8 py-1 focus:outline-none'
-                            placeholder='Search'
-                        />
-                    </div>
-                    <div className='profile bg-[#CCC] mr-[5vw] ml-[2vw] w-[30px] h-[30px] shadow rounded-full'>
-                    </div>
-                </div>                
-            </div>
+  return (
+    <div className="w-full bg-white drop-shadow-md">
+      <div className="md:ml-[25vw] ml-[5vw] flex items-center py-[1.5vh] align-center justify-between">
+        <div className="topic md:text-xl md:text-lg h-fit pl-[2vw]">
+          {props.title}
         </div>
-    );
+        <div className="flex items-center">
+          <div className="relative min-w-[2rem] md:max-w-[20rem]">
+            <img
+              src="images/search.svg"
+              alt="Description"
+              className="icon md:absolute left-3 top-2 object-cover object-center w-[1rem] h-[1.05rem]"
+              onClick={handleSearch}
+            />
+            <input
+              type="text"
+              className="bg-[#EEE] rounded-full md:block hidden px-8 py-1 focus:outline-none"
+              placeholder="Search"
+            />
+          </div>
+          <div className="profile bg-[#CCC] mr-[5vw] ml-[2vw] w-[30px] h-[30px] shadow rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
