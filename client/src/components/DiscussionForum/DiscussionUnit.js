ffa3974f8dbebf09840310d5b598c4bf9f53c1ad
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2024-02-10 02:59:49 +0530

    responsive--

--- client/src/components/DiscussionForum/DiscussionUnit.js
+++ client/src/components/DiscussionForum/DiscussionUnit.js
@@ -109,128 +109,142 @@ const DiscussionUnit = (props) => {
     <div className="w-full h-[14rem] text-black rounded-lg bg-white mb-2 drop-shadow-lg  mx-auto">
       <div className="flex md:flex-row md:p-3 md:pl-[2rem] pl-[1rem] mt-[1rem] md:mt-[1rem] flex-wrap">
         <div className="flex gap-12">
-          <Link to={`/profile/${props.discussion.poster && props.discussion.poster._id}`} className="flex items-center gap-2">
-          <div className="flex">
-            <div className="flex md:ml-auto">
-              <img
-                src={
-                  props.discussion.poster
-                    ? props.discussion.poster.profilePic ||
-                      "/images/defaultThumbnail.jpeg"
-                    : "/images/defaultThumbnail.jpeg"
-                }
-                alt="Profile"
-                className="w-[2.5rem] md:w-[3rem] h-[2.5rem] md:h-[3rem] rounded-full"
-              />
-            
-          </div>
-            </Link>
-        <div className="flex flex-col md:items-start md:justify-center md:ml-[1rem] ml-[0.1rem] items-center md:w-[39rem]">
-              <div className="flex gap-2">
-                <Link to={`/profile/${props.discussion.poster && props.discussion.poster._id}`} className="flex items-center gap-2">
-            <div className="md:text-[1rem] text-[0.8rem] pl-[0.5rem] font-semibold">
+          <Link
+            to={`/profile/${
+              props.discussion.poster && props.discussion.poster._id
+            }`}
+            className="flex items-center gap-2"
+          >
+            <div className="flex">
+              <div className="flex md:ml-auto">
+                <img
+                  src={
+                    props.discussion.poster
+                      ? props.discussion.poster.profilePic ||
+                        "/images/defaultThumbnail.jpeg"
+                      : "/images/defaultThumbnail.jpeg"
+                  }
+                  alt="Profile"
+                  className="w-[2.5rem] md:w-[3rem] h-[2.5rem] md:h-[3rem] rounded-full"
+                />
+              </div>
+            </div>
+          </Link>
+          <div className="flex flex-col md:items-start md:justify-center md:ml-[1rem] ml-[0.1rem] items-center md:w-[39rem] w-[10rem]">
+            <div className="flex gap-2  ml-[-6rem] ">
+              <Link
+                to={`/profile/${
+                  props.discussion.poster && props.discussion.poster._id
+                }`}
+                className="flex items-center gap-2"
+              >
+                <div className="md:text-[1rem] text-[0.8rem] font-semibold">
                   {props.discussion.poster && props.discussion.poster.name}
                 </div>
-                </Link>
-            <div className="flex items-center">
-                  <img
-                    src="images/verify.png"
-                    alt="Description"
-                    className="object-cover object-center w-[1.125rem] h-[1.125rem]"
-                  />
-                </div>
+              </Link>
+              <div className="flex items-center">
+                <img
+                  src="images/verify.png"
+                  alt="Description"
+                  className="object-cover object-center w-[1.125rem] h-[1.125rem]"
+                />
               </div>
-              <Link to={`/profile/${props.discussion.poster && props.discussion.poster._id}`} className="flex items-center gap-2">
-          <div className="text-[0.75rem] pl-[1rem] md:pl-0 text-[#0016DA]">
+            </div>
+            <Link
+              to={`/profile/${
+                props.discussion.poster && props.discussion.poster._id
+              }`}
+              className="flex items-center gap-2"
+            >
+              <div className="text-[0.75rem] ml-[-6rem] md:pl-0 text-[#0016DA]">
                 @{props.discussion.poster && props.discussion.poster.email}
               </div>
-              </Link>
-        </div>
-          </div>
-          <div className="flex md:col-span-3 flex-col align-top">
-            <div className="text-[0.875rem] text-[#0016DA]">{hoursAgo}</div>
+            </Link>
           </div>
         </div>
+        <div className="flex md:col-span-3 flex-col align-top">
+          <div className="text-[0.875rem] text-[#0016DA]">{hoursAgo}</div>
+        </div>
+      </div>
+      <div
+        className="w-full md:w-2/3 mt-[1rem] pl-1 hover:cusor-pointer"
+        onClick={() => {
+          window.location.href = `/discussionView?id=${props.discussion._id}`;
+        }}
+      >
+        <div className="text-[1rem] md:pl-[4rem] pl-[1rem]">
+          {props.discussion.content}
+        </div>
+      </div>
+      <div
+        className="flex justify-end md:col-span-3 items-center"
+        onClick={() => {
+          window.location.href = `/discussionView?id=${props.discussion._id}`;
+        }}
+      ></div>
+      <div className="w-full md:w-2/3 flex  gap-1 text-black px-2 pt-[3rem]">
+        <textarea
+          onChange={handleTextAreaChange}
+          onFocus={() => {
+            setSubmit(true);
+          }}
+          onBlur={handleBlur}
+          rows={1}
+          type="text"
+          placeholder="Answer Here"
+          className="overflow-hidden  shadow py-[0.375rem] bg-[#E5E5E5] text-black
+                            placeholder:font-semibold p-3 rounded-2xl w-[150%] focus:border-[#0016DA] focus:outline-none 
+                            focus:border resize-none"
+        />
+        <div className={`${submit ? "block" : "hidden"}`}>
+          <button
+            className="bg-[#0016DA] shadow ml-auto text-white text-[12px] font-bold rounded-lg px-4 py-1"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <div
+        className="md:col-span-3"
+        onClick={() => {
+          window.location.href = `/discussionView?id=${props.discussion._id}`;
+        }}
+      ></div>
+      <div
+        className="md:col-span-1"
+        onClick={() => {
+          window.location.href = `/discussionView?id=${props.discussion._id}`;
+        }}
+      ></div>
+      <div
+        className="md:col-span-8"
+        onClick={() => {
+          window.location.href = `/discussionView?id=${props.discussion._id}`;
+        }}
+      ></div>
+      <div className="w-full md:w-1/4 flex h-fit gap-4 md:mt-[3rem] justify-end text-black align-center items-center py-0.5 px-2 rounded-full border-[1px] border-white">
         <div
-          className="w-full md:w-2/3 mt-[1rem] pl-1 hover:cusor-pointer"
+          className="flex gap-1 items-center"
           onClick={() => {
             window.location.href = `/discussionView?id=${props.discussion._id}`;
           }}
         >
-          <div className="text-[1rem] md:pl-[4rem] pl-[1rem]">
-            {props.discussion.content}
-          </div>
-        </div>
-        <div
-          className="flex justify-end md:col-span-3 items-center"
-          onClick={() => {
-            window.location.href = `/discussionView?id=${props.discussion._id}`;
-          }}
-        ></div>
-        <div className="w-full md:w-2/3 flex  gap-1 text-black px-2 pt-[3rem]">
-          <textarea
-            onChange={handleTextAreaChange}
-            onFocus={() => {
-              setSubmit(true);
-            }}
-            onBlur={handleBlur}
-            rows={1}
-            type="text"
-            placeholder="Answer Here"
-            className="overflow-hidden  shadow py-[0.375rem] bg-[#E5E5E5] text-black
-                            placeholder:font-semibold p-3 rounded-2xl w-[150%] focus:border-[#0016DA] focus:outline-none 
-                            focus:border resize-none"
+          <img
+            src={"images/comment.svg"}
+            alt="Description"
+            className="object-cover object-center w-[0.98rem] h-[0.98rem]"
           />
-          <div className={`${submit ? "block" : "hidden"}`}>
-            <button
-              className="bg-[#0016DA] shadow ml-auto text-white text-[12px] font-bold rounded-lg px-4 py-1"
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
-          </div>
+          <div className="text-[0.875rem]">{comments}</div>
         </div>
-        <div
-          className="md:col-span-3"
-          onClick={() => {
-            window.location.href = `/discussionView?id=${props.discussion._id}`;
-          }}
-        ></div>
-        <div
-          className="md:col-span-1"
-          onClick={() => {
-            window.location.href = `/discussionView?id=${props.discussion._id}`;
-          }}
-        ></div>
-        <div
-          className="md:col-span-8"
-          onClick={() => {
-            window.location.href = `/discussionView?id=${props.discussion._id}`;
-          }}
-        ></div>
-        <div className="w-full md:w-1/4 flex h-fit gap-4 md:mt-[3rem] justify-end text-black align-center items-center py-0.5 px-2 rounded-full border-[1px] border-white">
-          <div
-            className="flex gap-1 items-center"
-            onClick={() => {
-              window.location.href = `/discussionView?id=${props.discussion._id}`;
-            }}
-          >
-            <img
-              src={"images/comment.svg"}
-              alt="Description"
-              className="object-cover object-center w-[0.98rem] h-[0.98rem]"
-            />
-            <div className="text-[0.875rem]">{comments}</div>
-          </div>
-          <div className="flex gap-1 align-center items-center">
-            <img
-              src={`images/${upvoted ? "upvote" : "emptyUpvote"}.svg`}
-              alt="Description"
-              className="object-cover object-center w-[0.875rem] h-[0.875rem]"
-              onClick={handleUpvote}
-            />
-            <div className="text-[0.875rem]">{upvotes}</div>
-          </div>
+        <div className="flex gap-1 align-center items-center">
+          <img
+            src={`images/${upvoted ? "upvote" : "emptyUpvote"}.svg`}
+            alt="Description"
+            className="object-cover object-center w-[0.875rem] h-[0.875rem]"
+            onClick={handleUpvote}
+          />
+          <div className="text-[0.875rem]">{upvotes}</div>
         </div>
       </div>
     </div>
